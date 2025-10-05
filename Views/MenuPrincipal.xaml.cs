64c64acbe8bc071212f4f9c0c8266756fa4a8a4c
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-10-05 14:24:53 -0300

    feat: Agrego code-behind de la View MenuPrincipal (Contiene BindingContext)

--- Views/MenuPrincipal.xaml.cs
+++ Views/MenuPrincipal.xaml.cs
@@ -1,9 +1,11 @@
 namespace TrainiumNeon.Views;
+using TrainiumNeon.ViewModels;
 
 public partial class MenuPrincipal : ContentPage
 {
 	public MenuPrincipal()
 	{
 		InitializeComponent();
+		BindingContext = new MenuPrincipalViewModel();
 	}
 }
