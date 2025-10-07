c3e217202d3f60d78a537190f74db6a6dea808be
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-10-06 21:51:20 -0300

    feat: Registro rutas para nuevas Views

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace TrainiumNeon
+﻿using TrainiumNeon.Views;
+
+namespace TrainiumNeon
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(DetalleEjercicio), typeof(DetalleEjercicio));
+            Routing.RegisterRoute(nameof(AgregarEditarRutina), typeof(AgregarEditarRutina));
+            Routing.RegisterRoute(nameof(AgregarEjercicioRutina), typeof(AgregarEjercicioRutina));
         }
     }
 }
