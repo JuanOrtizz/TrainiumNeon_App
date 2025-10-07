a4b403a0640f6dddd55287b5da1f3630ac77956f
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-10-06 21:49:55 -0300

    feat: Agrego diseño (XAML) y archivo Code-Behind de la View AgregarEjercicioRutina

---
+++ Views/AgregarEjercicioRutina.xaml.cs
@@ -0,0 +1,10 @@
+namespace TrainiumNeon.Views;
+
+public partial class AgregarEjercicioRutina : ContentPage
+{
+	public AgregarEjercicioRutina()
+	{
+		InitializeComponent();
+		BindingContext = new AgregarEjercicioRutinaViewModel();
+	}
+}
