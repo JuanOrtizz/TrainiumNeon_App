14f83ac5de04398fa0b940cf2549f70b12982fd1
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-10-06 21:56:04 -0300

    feat: Agrego ViewModel Rutinas (WIP)

--- ViewModels/RutinasViewModel.cs
+++ ViewModels/RutinasViewModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainiumNeon.Views;
 
 namespace TrainiumNeon.ViewModels
 {
-    internal class RutinasViewModel
+    public class RutinasViewModel
     {
+        public Command AgregarRutinaCommand { get; }
+
+        public RutinasViewModel()
+        {
+            AgregarRutinaCommand = new Command(async () => await NavegarAgregarRutina());
+        }
+
+        private async Task NavegarAgregarRutina()
+        {
+            await Shell.Current.GoToAsync(nameof(AgregarEditarRutina));
+        }
     }
 }
