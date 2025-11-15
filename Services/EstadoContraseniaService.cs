8cc9b52fda25749da26896daf3bff99aedd87bda
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-11-15 12:57:05 -0300

    feat: Agrego Servicio 'EstadoContraseniaService'

---
+++ Services/EstadoContraseniaService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainiumNeon.Services
+{
+    public class EstadoContraseniaService : IEstadoContraseniaService
+    {
+        public (bool contraseniaOculta, string iconoContrasenia) CambiarEstadoContrasenia(bool contraseniaOculta, string iconoContrasenia)
+        {
+            contraseniaOculta = !contraseniaOculta;
+            if (contraseniaOculta)
+            {
+                iconoContrasenia = "ver_contrasenia.png";
+            }
+            else
+            {
+                iconoContrasenia = "no_ver_contrasenia.png";
+            }
+            return (contraseniaOculta, iconoContrasenia);
+        }
+    }
+}
