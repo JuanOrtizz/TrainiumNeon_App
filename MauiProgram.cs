0fe7be02559f344219d6ab9aeb55e4ebb377843a
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-11-15 13:03:56 -0300

    feat: Agrego registros de dependencias (Servicios, ViewModels y Views) en DI

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
 using CommunityToolkit.Maui;
+using TrainiumNeon.Services;
+using TrainiumNeon.ViewModels;
+using TrainiumNeon.Views;
 
 namespace TrainiumNeon
 {
@@ -16,9 +19,35 @@ namespace TrainiumNeon
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            //Servicios
+            builder.Services.AddHttpClient<IApiEjerciciosService, ApiEjerciciosService>();
+            builder.Services.AddSingleton<IValidacionService, ValidacionService>();
+            builder.Services.AddSingleton<IEstadoContraseniaService, EstadoContraseniaService>();
+
+            // ViewModels
+            builder.Services.AddTransient<DetalleEjercicioViewModel>();
+            builder.Services.AddTransient<AgregarEjercicioRutinaViewModel>();
+            builder.Services.AddTransient<AgregarEditarRutinaViewModel>();
+            builder.Services.AddTransient<EstadisticasViewModel>();
+            builder.Services.AddTransient<MenuPrincipalViewModel>();
+            builder.Services.AddTransient<PerfilViewModel>();
+            builder.Services.AddTransient<RegistroViewModel>();
+            builder.Services.AddTransient<RutinasViewModel>();
+            builder.Services.AddTransient<MainPageViewModel>();
+
+            // Views
+            builder.Services.AddTransient<DetalleEjercicio>();
+            builder.Services.AddTransient<AgregarEjercicioRutina>();
+            builder.Services.AddTransient<AgregarEditarRutina>();
+            builder.Services.AddTransient<Estadisticas>();
+            builder.Services.AddTransient<MenuPrincipal>();
+            builder.Services.AddTransient<Perfil>();
+            builder.Services.AddTransient<Registro>();
+            builder.Services.AddTransient<Rutinas>();
+            builder.Services.AddTransient<MainPage>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
