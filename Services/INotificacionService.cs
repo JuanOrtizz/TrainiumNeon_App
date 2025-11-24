5293ca5d6194d77d0947b030569b2e272ed6c73d
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-11-24 18:11:41 -0300

    feat: Agrego Interfaz y Servicio de Notificacion

---
+++ Services/INotificacionService.cs
@@ -0,0 +1,7 @@
+﻿namespace TrainiumNeon.Services
+{
+    public interface INotificacionService
+    {
+        void EnviarNotificacion(string titulo, string mensaje, DateTime? programarHorario = null, bool repetirDiariamente = false);
+    }
+}
---
+++ Services/NotificacionService.cs
@@ -0,0 +1,25 @@
+﻿using Plugin.LocalNotification;
+
+namespace TrainiumNeon.Services
+{
+    public class NotificacionService : INotificacionService
+    {
+        public void EnviarNotificacion(string titulo, string mensaje, DateTime? programarHorario = null, bool repetirDiariamente = false)
+        {
+            var programarNotificacion = programarHorario.HasValue ? new NotificationRequestSchedule
+            {
+                NotifyTime = programarHorario.Value,
+                RepeatType = repetirDiariamente ? NotificationRepeat.Daily : NotificationRepeat.No
+            }: null;
+
+            var notification = new NotificationRequest
+            {
+                Title = titulo,
+                Description = mensaje,
+                Schedule = programarNotificacion
+            };
+
+            LocalNotificationCenter.Current.Show(notification);
+        }
+    }
+}
