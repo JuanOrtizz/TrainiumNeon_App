c6c1f9dea35693635d714679fb7c876d9c7cebeb
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-11-24 05:21:46 -0300

    refactor: Actualizo App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,42 @@
-﻿using TrainiumNeon.Views;
+﻿using TrainiumNeon.Services;
+using TrainiumNeon.Views;
 
 namespace TrainiumNeon
 {
     public partial class App : Application
     {
-        public App(MainPage mainPage)
+        // Campos de solo lectura para DI
+        private readonly MainPage _mainPage;
+        private readonly ISesionService _sesionService;
+
+        public App(MainPage mainPage, ISesionService sesionService)
         {
             InitializeComponent();
-            MainPage = new NavigationPage(mainPage);
+            // Inicializan los campos con DI
+            _mainPage = mainPage;
+            _sesionService = sesionService;
         }
+
+        // Sobrescribo el metodo CreateWindow para inicializar Shell
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+
+        // Sobrescribo le metodo OnStart para decidir en que view arranca dependiendo si esta logeado o no
+        protected override void OnStart()
+        {
+            var id = _sesionService.ObtenerSesion();
+
+            if (id != 0)
+            {
+                Shell.Current?.GoToAsync("//MenuPrincipal");
+            }
+            else
+            {
+                Shell.Current?.GoToAsync("//Login");
+            }
+        }
+
     }
 }
