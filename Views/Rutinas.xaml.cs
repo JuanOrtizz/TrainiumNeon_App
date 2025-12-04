e651fc90e7c4654fbdf1cac669cf975a81f8a431
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-12-04 01:47:49 -0300

    feat: Agrego OnApering con Bandera para cargar datos al iniciar la Page Rutinas

--- Views/Rutinas.xaml.cs
+++ Views/Rutinas.xaml.cs
@@ -4,9 +4,25 @@ namespace TrainiumNeon.Views;
 
 public partial class Rutinas : ContentPage
 {
+    private bool _inicializado = false;
+
 	public Rutinas(RutinasViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (!_inicializado)
+        {
+            _inicializado = true;
+            if (BindingContext is RutinasViewModel vm)
+            {
+                await vm.InicializarAsync();
+            }
+        }
+    }
 }
