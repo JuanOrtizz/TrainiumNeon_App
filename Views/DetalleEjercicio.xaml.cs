d3718bbb1864e8d438b4f339ac9d10d453fd2695
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-12-04 01:37:21 -0300

    feat: Agrego OnApering con Bandera para cargar datos al iniciar la Page DetalleEjercicio

--- Views/DetalleEjercicio.xaml.cs
+++ Views/DetalleEjercicio.xaml.cs
@@ -4,9 +4,25 @@ namespace TrainiumNeon.Views;
 
 public partial class DetalleEjercicio : ContentPage
 {
-	public DetalleEjercicio(DetalleEjercicioViewModel vm)
+    private bool _inicializado = false;
+
+    public DetalleEjercicio(DetalleEjercicioViewModel vm)
 	{
 		InitializeComponent();
         BindingContext = vm;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (!_inicializado)
+        {
+            _inicializado = true;
+            if (BindingContext is DetalleEjercicioViewModel vm)
+            {
+                await vm.InicializarAsync();
+            }
+        }
     }
 }
