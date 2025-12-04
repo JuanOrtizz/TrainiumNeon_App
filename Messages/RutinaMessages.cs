7abbecf513fba227195619282d4102d914cd1bd5
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-12-04 01:01:37 -0300

    feat: Agrego 'RutinaMessages' para la mensajeria de los VMs

---
+++ Messages/RutinaMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainiumNeon.Messages
+{
+    public class RutinaMessages
+    {
+        public record RutinaCreadaMessage(string mensaje);
+        public record RutinaActualizadaMessage(string mensaje);
+        public record RutinaEliminadaMessage(string mensaje);
+        public record RutinaSeleccionadaActualizadaMessage(string mensaje);
+
+    }
+}
