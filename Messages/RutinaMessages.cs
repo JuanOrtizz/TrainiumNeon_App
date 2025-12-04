9ad5d9bb3858c18dba5338afd2461335c9e5ec53
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-12-04 02:02:18 -0300

    refactor: Actualizo RutinaMessages

--- Messages/RutinaMessages.cs
+++ Messages/RutinaMessages.cs
@@ -2,8 +2,7 @@
 {
     public class RutinaMessages
     {
-        public record RutinaCreadaMessage(string mensaje);
-        public record RutinaActualizadaMessage(string mensaje);
+        public record RutinaGuardadaMessage(string mensaje);
         public record RutinaEliminadaMessage(string mensaje);
         public record RutinaSeleccionadaActualizadaMessage();
 
