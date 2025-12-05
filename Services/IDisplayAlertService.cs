894ecac98d6cc95d4df21b4e87672e4f3b100620
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-12-05 01:40:18 -0300

    feat: Agrego servicio de DisplayAlert

---
+++ Services/IDisplayAlertService.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainiumNeon.Services
+{
+    public interface IDisplayAlertService
+    {
+        Task MostrarAlertAsync(string titulo, string mensaje, string cancelar);
+        Task<bool> MostrarAlertConConfirmacionAsync(string titulo, string mensaje, string aceptar, string cancelar);
+    }
+
+}
